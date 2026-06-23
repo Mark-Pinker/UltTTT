f7461f2050a9afa8add5ac5ecab6cf59e54869a0
Mark-Pinker <mpinkrahsr@gmail.com>
2026-06-23 17:44:41 -0400

    Updated Game and main Files

--- Game.java
+++ Game.java
@@ -38,4 +38,166 @@ public class Game{
             System.out.println();
         }
     }
+    public static boolean move(int player,int large, int small){
+        System.out.print("\033[H\033[2J");
+        String text = "  ";
+        int sRow = 0;
+        int sCol = 0;
+        int lRow = 0;
+        int lCol = 0;
+        if (large <= 3){
+            lRow = 0;
+        }else if(large > 6){
+            lRow = 2;
+        }else{
+            lRow = 1;
+        }
+       lCol = (large%3)-1;
+       if (lCol == -1){
+            lCol = 2;
+       }
+        
+        if (small <= 3){
+            sRow = 0;
+        }else if(small > 6){
+            sRow = 2;
+        }else{
+            sRow = 1;
+        }
+        sCol = (small%3)-1;
+        if (sCol == -1){
+            sCol = 2;
+        }
+        if (player ==1){
+            text = " x ";
+        }else{
+            text = " o ";
+        }
+        if (board[lRow][lCol][sRow][sCol].equals(" - ")){
+            board[lRow][lCol][sRow][sCol] = text;
+            printBoard();
+            if (entireWins(player)){
+                System.out.println("Player " + player + " has won the game!");
+            }
+            System.out.print("\033[H\033[2J");
+            return true;
+        }
+
+        return false;
+    }
+    public static boolean checkFullness(int large){
+        int lRow = 0;
+        int lCol = 0;
+        if (large <= 3){
+            lRow = 0;
+        }else if(large > 6){
+            lRow = 2;
+        }else{
+            lRow = 1;
+        }
+       lCol = (large%3)-1;
+       if (lCol == -1){
+            lCol = 2;
+       }
+        String check = board[lRow][lCol][0][0];
+        for (int outer = 0; outer <= 2;outer++){
+            for(int inner = 0; inner <= 2; inner++){
+                if (board[lRow][lCol][outer][inner].equals(" - ")){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static boolean smallWins(int player, int outRow, int outCol){
+        String text = " - ";
+        if (player == 1){
+            text = " x ";
+        }else{
+            text = " o ";    
+        }
+        boolean win = false;
+        for (int inRow = 0; inRow < 3; inRow++){
+            int count = 0;
+            for (int inCol = 0; inCol < 3; inCol++){
+                if(board[outRow][outCol][inRow][inCol].equals(text)){
+                    count++;
+                }else{
+                    count = 0;
+                }
+                if (count>=3){
+                    win = true;
+                }
+            }
+        }
+        for (int inCol = 0; inCol < 3; inCol++){
+            int count = 0;
+            for (int inRow = 0; inRow < 3; inRow++){
+                if(board[outRow][outCol][inRow][inCol].equals(text)){
+                    count++;
+                }else{
+                    count = 0;
+                }
+                if (count>=3){
+                    win = true;
+                }
+            }
+        }
+        if (board[outRow][outCol][2][2].equals(text) && board[outRow][outCol][1][1].equals(text) && board[outRow][outCol][0][0].equals(text)){
+            win = true;
+        }
+        if (board[outRow][outCol][2][0].equals(text) && board[outRow][outCol][1][1].equals(text) && board[outRow][outCol][0][2].equals(text)){
+            win = true;
+        }
+        if (win == true){
+                for (int sRow = 0; sRow < 3; sRow++){
+                    for (int sCol = 0; sCol < 3; sCol++){
+                        board[outRow][outCol][sRow][sCol] = text;
+                    }
+                }
+            return true;
+        }
+        return false;
+    }
+    public static boolean entireWins(int player){
+        String text;
+        if (player == 1){
+            text = " x ";
+        }else{
+            text = " o ";    
+        }
+        for (int lRow = 0; lRow < 3; lRow++){
+            int count = 0;
+            for (int lCol = 0; lCol < 3; lCol++){
+                if(smallWins(player,lRow,lCol)){
+                    count++;
+                }else{
+                    count = 0;
+                }
+                if (count>=3){
+                    return true;
+                }
+            }
+        }
+        for (int lCol = 0; lCol < 3; lCol++){
+            int count = 0;
+            for (int lRow = 0; lRow < 3; lRow++){
+                if(smallWins(player,lRow,lCol)){
+                    count++;
+                }else{
+                    count = 0;
+                }
+                if (count>=3){
+                    return true;
+                }
+            }
+        }
+        if (smallWins(player, 0,0) && smallWins(player,1,1) && smallWins(player,2,2)){
+            return true;
+        }
+        if (smallWins(player, 0,2) && smallWins(player,1,1) && smallWins(player,2,0)){
+            return true;
+        }
+        return false;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,41 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 //import java.util.ArrayList;
 //import java.lang.Thread;
 public class Main {
     public static void main(String[] args) {
-        Game man = new Game();
-        Game.printBoard();
+        boolean gaming = true;
+        int turns = 1;
+        Game board = new Game();
+        int small = 0;
+        int large = 0;
+        System.out.println("Welcome to Ultimate Tic Tac Toe!");
+        boolean boardChoice = true;
+        int player = 0;
+        Scanner input = new Scanner(System.in);
+        
+        while (gaming = true){
+            player = turns%2;
+            Game.printBoard();
+            if(boardChoice == true){
+                System.out.println("Which Tic Tac Toe Board do want to go in. \n Formatted as \n [1] [2] [3] \n [4] [5] [6] \n [7] [8] [9]");
+                large = input.nextInt();
+                System.out.print("\033[H\033[2J");
+            }
+            System.out.println("You are going in Tic Tac Toe Board " + large);
+            Game.printBoard();
+            System.out.println("What box do want to go in. \n Formatted as \n [1] [2] [3] \n [4] [5] [6] \n [7] [8] [9]");
+            small = input.nextInt();
+            System.out.print("\033[H\033[2J");
+            Game.move(player,large, small);
+            large = small;
+            if(Game.checkFullness(large)){
+                boardChoice = true;
+            }else{
+                boardChoice = false;
+            }
+            turns++;
+        }
+
 
     }
 }
